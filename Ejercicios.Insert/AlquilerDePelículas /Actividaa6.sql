a5e8bc047dae6cd0c4e7eefe2ae06ddd8cdc6b4c
CarolinaWendyVela <velacarolina52@gmail.com>
2023-10-05 12:11:59 -0300

    AlquilerDePeliculas

---
+++ Ejercicios.Insert/AlquilerDePelículas /Actividaa6.sql
@@ -0,0 +1,10 @@
+SELECT f.title as peliculas, a.address AS direccion, 
+c.city AS ciudad, co.country AS pais FROM film f 
+JOIN inventory i ON f.film_id = i.film_id
+JOIN store s ON i.store_id = s.store_id
+JOIN address a ON s.address_id = a.address_id
+JOIN city c ON a.city_id = c.city_id
+JOIN country co ON c.country_id = co.country_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+WHERE r.return_date IS NULL
+ORDER BY f.title;
